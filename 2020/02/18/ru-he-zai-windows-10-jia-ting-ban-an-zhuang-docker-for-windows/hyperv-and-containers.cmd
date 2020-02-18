aaa93cdfd4ef3df1fe7816978fe0057c2fad9b9e
Shuduo Sang <sangshuduo@gmail.com>
2020-02-18 23:14:02 +0800

    Site updated: 2020-02-18 23:14:02

---
+++ 2020/02/18/ru-he-zai-windows-10-jia-ting-ban-an-zhuang-docker-for-windows/hyperv-and-containers.cmd
@@ -0,0 +1,18 @@
+pushd "%~dp0"
+ 
+dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
+ 
+for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+ 
+del hyper-v.txt
+ 
+Dism /online /enable-feature /featurename:Microsoft-Hyper-V -All /LimitAccess /ALL
+
+
+dir /b %SystemRoot%\servicing\Packages\*containers*.mum >containers.txt
+ 
+for /f %%i in ('findstr /i . containers.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+ 
+del containers.txt
+ 
+Dism /online /enable-feature /featurename:Containers -All /LimitAccess /ALL
